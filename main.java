b42de9d98fc9776279ad26317be207d09e8750b0
Caner <57899804+canermastan@users.noreply.github.com>
2021-06-04 10:47:26 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Main{
+  public static void main(String[] args){
+    // ax^2 + bx + c 
+    Scanner scanner = new Scanner(System.in);
+    double a,b,c;
+    System.out.println("Insert value for a: ");
+    a = scanner.nextFloat();
+    System.out.println("Insert value for b: ");
+    b = scanner.nextFloat();
+    System.out.println("Insert value for c: ");
+    c = scanner.nextFloat();
+    double discriminant = Math.pow(b,2) - 4*a*c;
+    if (discriminant > 0){
+      double root1 = (-b + Math.sqrt(discriminant)) / 2*a;
+      double root2 = (-b - Math.sqrt(discriminant)) / 2*a;
+      System.out.println("Root 1 : "+root1);
+      System.out.println("Root 2 : "+root2);
+    }else if (discriminant == 0){
+      System.out.println("Root : " + (-b / 2*a));
+    }else{
+      System.out.println("The equation has no real roots.");
+    }
+  }
+}
